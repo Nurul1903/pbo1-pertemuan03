58ecb2632e708b3a4a2f01834af14308846dbed3
Nurul1903 <amatidanurulhida@gmail.com>
2021-04-11 14:54:16 +0700

    Percabangan if else

---
+++ src/Percabangan_if_else.java
@@ -0,0 +1,14 @@
+package percabngan;
+
+public class Percabangan_if_else {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
+
+        if (uangDiDompet<totalBelanja){
+            System.out.println("uang kurang, kurangi jajan anda");
+        }else{
+            System.out.println("uang cukup. selamat menikmati");
+        }
+    }
+}
